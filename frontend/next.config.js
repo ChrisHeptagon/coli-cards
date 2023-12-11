727863a76641a5cbbc253ad9ba8c59fb2d06734c
ChrisHeptagon <ccollymore348@gmail.com>
2023-12-11 10:17:21 -0500

    Attempt #1 of fixing HMR

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+      ) => {
+        config.devServer = {
+            client: {
+                hostname: 'localhost',
+                pathname: '/ws',
+                port: 8500,
+                protocol: 'ws',
+            }
+        }
+        return config
+      },
+    }
 
 module.exports = nextConfig
